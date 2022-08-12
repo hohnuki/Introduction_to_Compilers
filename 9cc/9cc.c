7a47c7286b51418b9d60e8b1905b6112a1d4a098
hohnuki <ohnukihiroki8585@yahoo.co.jp>
2022-08-12 23:15:17 +0900

    [add]:9cc.cに字句解析をかき始めた

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -1,5 +1,68 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// トークンの種類
+typedef enum {
+	TK_RESERVED, // 記号
+	TK_NUM,      // 整数トークン
+	TK_EOF,      // 入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+// トークン型
+struct Token {
+	TokenKind kind; // トークンの型
+	Token *next;    // 次の入力トークン
+	int val;        // kindがTK_NUMの場合、その数値
+	char *str;      // トークン文字列
+};
+
+// 現在着目しているトークン
+Token *token;
+
+// エラーを報告するための関数
+// printfと同じ引数を取る
+void error(char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
+// 次のトークンが期待している記号のときには、トークンを1つ読み進める。
+// それ以外の場合にはエラーを報告する。
+void expect(char op) {
+	if (token->kind != TK_RESERVED || token->str[0] != op)
+		error("'%c'ではありません", op);
+	token = token->next;
+}
+
+bool at_eof() {
+	return token->kind == TK_EOF;
+}
+
+// 新しいトークンを作成してcurに繋げる
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+	Token *tok = calloc(1, sizeof(Token));
+	tok->kind = kind;
+	tok->str = str;
+	cur->next = tok;
+	return tok;
+}
+
+Token *tokenize(char *p) {
+	Token head;
+	head.next = NULL;
+	Token *cur = &head;
+
+
+}
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
